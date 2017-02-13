49ade95943bb8fef99fc4f29aee327990718ca74
Lee Saville <l.w.saville@gmail.com>
2017-02-13 14:31:42 -0700

    Change expectations to have_content rather than buttons.

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -3,17 +3,20 @@ require 'rails_helper'
 describe 'user logs in' do
   context 'they visit login_path and enter appropriate credentials' do
     scenario 'they are redirected to links_path and receive a success flash' do
-      user = FactoryGirl.create(:user, password: 'blah')
+      user = User.create(email: 'o@bama.com', password: 'michelle')
+
       visit login_path
 
       fill_in "Email", with: user.email
-      fill_in "Password", with: 'blah'
-      click_on "Login"
+      fill_in "Password", with: 'michelle'
+      within 'form' do
+        click_on "Login"
+      end
 
       expect(current_path).to eq(links_path)
       expect(page).to have_content("Successfully logged in!")
-      expect(page).to have_button("Logout")
-      expect(page).to_not have_button("Login")
+      expect(page).to have_content("Logout")
+      expect(page).to_not have_content("Login")
     end
   end
 end
