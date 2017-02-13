572c8891254ecab129f42be669d0ab1490f4058b
Lee Saville <l.w.saville@gmail.com>
2017-02-13 13:03:05 -0700

    Add faker to factory girl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    email "MyString"
-    password_digest "MyString"
+    email { Faker::Internet.email }
+    password { Faker::Hipster.word }
   end
 end
