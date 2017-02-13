ac24ee04bfb9d9d2d87e99f46f8762e5416a64ee
Lee Saville <l.w.saville@gmail.com>
2017-02-13 13:40:18 -0700

    Add logic for creation of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Successfully created account!"
+      session[:user_id] = @user.id
+      redirect_to links_path
+    else
+      render :new
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
