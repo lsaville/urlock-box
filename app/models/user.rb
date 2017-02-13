ac4545ee8a3e01a0a46a13f723f2aa3de3ed01a7
Lee Saville <l.w.saville@gmail.com>
2017-02-13 15:29:01 -0700

    Add relationship has_many links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   validates :email, :password, presence: true
   validates :email, uniqueness: true
   validates_confirmation_of :password
 
+  has_many :links
 end
