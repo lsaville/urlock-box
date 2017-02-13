d55f51405735d16d3e35e519194ec8a2944df359
Lee Saville <l.w.saville@gmail.com>
2017-02-13 12:45:34 -0700

    Write initial test for user creation on new_user_path

---
+++ spec/features/user_can_create_account_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'visitor goes to new_user_path' do
+  context 'they enter all required fields' do
+    scenario 'they are redirected to links_path and see successful flash' do
+      visit new_user_path
+
+      fill_in "Email", with: 'bub@example.com'
+      fill_in "Password", with: 'pass'
+      fill_in "Password confirmation", with: 'pass'
+
+      expect(page).to have_content("Successfully created account!")
+      expect(page).to have_content("Logout")
+      expect(page).to_not have_content("Login")
+    end
+  end
+end
