a0c24e81e29c4256e27888b13eee41748edce9bd
Lee Saville <l.w.saville@gmail.com>
2017-02-13 16:00:12 -0700

    Utilize helper method defined in rails_helper to log obama in

--- spec/features/user_sees_form_for_new_link_on_links_index_spec.rb
+++ spec/features/user_sees_form_for_new_link_on_links_index_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 describe 'user visits links index' do
   scenario 'they see a form to create a link' do
-    user = User.create(email: 'o@bama.com', password: 'michelle')
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
+    user_logs_in
     visit links_path
 
     within('form') do
