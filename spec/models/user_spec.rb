a75a286464735d9bcf9a3d6b3906324d1835e85a
Lee Saville <l.w.saville@gmail.com>
2017-02-13 13:07:36 -0700

    Add test for email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'validations' do
     it 'validates presence of email' do
-      
+      user = FactoryGirl.build(:user, email: nil)
+
+      expect(user).to_not be_valid
     end
   end
 end
