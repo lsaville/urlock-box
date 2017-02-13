a09f6b795a3463ce33a09881549619d7b45841b9
Lee Saville <l.w.saville@gmail.com>
2017-02-13 15:58:51 -0700

    Add create action for Link Add the Link to the current_users links Set up link_params to circumvent strong params

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,4 +4,21 @@ class LinksController < ApplicationController
   def index
     @link = Link.new
   end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      current_user.links << @link
+      flash[:success] = "Successfully created link!"
+      redirect_to links_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:url, :title)
+    end
 end
