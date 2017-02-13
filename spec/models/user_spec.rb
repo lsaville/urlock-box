e71b7d775e0bef6d41de9fd0eea278944a4ea054
Lee Saville <l.w.saville@gmail.com>
2017-02-13 13:15:53 -0700

    Add testing for presence of password, uniqueness of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,18 @@ RSpec.describe User, type: :model do
 
       expect(user).to_not be_valid
     end
+
+    it 'validates presence of password' do
+      user = FactoryGirl.build(:user, password: nil)
+
+      expect(user).to_not be_valid
+    end
+
+    it 'validate uniqueness of email' do
+      FactoryGirl.create(:user, email: 'bub@example.com')
+      user = FactoryGirl.build(:user, email: 'bub@example.com')
+
+      expect(user).to_not be_valid
+    end
   end
 end
