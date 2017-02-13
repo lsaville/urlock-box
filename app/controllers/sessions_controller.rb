7c5a11be48818807ea92689bc8f14c2ea1fc51cd
Lee Saville <l.w.saville@gmail.com>
2017-02-13 14:30:00 -0700

    Add logic to sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by_email(params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      flash[:sucesss] = "Successfully logged in!"
+      session[:user_id] = @user.id
+      redirect_to links_path
+    else
+      flash[:failure] = "Something went wrong!"
+      render :new
+    end
+  end
+
+
 end
