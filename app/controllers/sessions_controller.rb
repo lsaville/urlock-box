99d0b50b8ddd1a2a7023c7cc1415c07ddf326206
Lee Saville <l.w.saville@gmail.com>
2017-02-13 14:46:47 -0700

    Create destory action with session delete and redirect to login_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,9 @@ class SessionsController < ApplicationController
     end
   end
 
-
+  def destroy
+    session.delete(:user_id)
+    flash[:success] = "Successfully logged out!"
+    redirect_to login_path
+  end
 end
